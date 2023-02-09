797a0bc020e5d80c4846cb9edba4040568231bf6
Siroshun09 <siro.shun8@gmail.com>
2023-02-10 03:00:02 +0900

    build: move v4 to root

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,51 @@
-group = "net.okocraft.boxmigrator"
-version = "1.0-SNAPSHOT"
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+version = "v4"
+
+repositories {
+    mavenCentral()
+
+    maven {
+        url = uri("https://papermc.io/repo/repository/maven-public/")
+    }
+
+    maven {
+        url = uri("https://okocraft.github.io/Box/maven/")
+    }
+}
+
+dependencies {
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("com.github.siroshun09.configapi:configapi-yaml:4.6.0")
+
+    compileOnly("net.okocraft.box:api:4.4.1")
+    compileOnly("net.okocraft.box:autostore:4.4.1")
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+    compileOnly("org.jetbrains:annotations:24.0.0")
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    jar {
+        archiveFileName.set("BoxMigrator.jar")
+    }
+
+    processResources {
+        filesMatching("plugin.yml") {
+            expand("projectVersion" to project.version)
+        }
+    }
+
+    shadowJar {
+        minimize()
+        archiveFileName.set("BoxMigrator.jar")
+        relocate("com.zaxxer", "net.okocraft.boxmigrator.lib")
+        relocate("com.github.siroshun09", "net.okocraft.boxmigrator.lib")
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,1 @@
-rootProject.name = "boxmigrator"
-
-// Box versions
-sequenceOf(
-    "v4" // v3 to v4
-).forEach {
-    include(it)
-    project(":$it").projectDir = file(it)
-}
+rootProject.name = "BoxMigrator"
